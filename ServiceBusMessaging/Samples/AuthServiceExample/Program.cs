714ee832b47a846ac8440f4478693968a75f9aa8
GitHub4Jimmy <79417331+GitHub4Jimmy@users.noreply.github.com>
2022-04-21 14:57:40 -0400

    Add files via upload

---
+++ ServiceBusMessaging/Samples/AuthServiceExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using StagwellTech.ServiceBusRPC;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AuthServiceExample
+{
+    class Program
+    {
+        static string ServiceBusConnectionString = "Endpoint=######################################################";
+
+        static void Main(string[] args)
+        {
+
+            new ServiceBusRPCService(ServiceBusConnectionString);
+            while (true)
+            {
+                Thread.Sleep(10000);
+            }
+        }
+
+        [ServiceBusRPCService(queueName: "sample-service")]
+        public static string checkForAuthorization(string authRequest)
+        {
+            Console.WriteLine($"Received auth request - {authRequest}. Returning YES");
+            return "YES";
+        }
+    }
+}
