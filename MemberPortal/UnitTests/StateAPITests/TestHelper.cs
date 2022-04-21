a9107ec99b1815e8433ec61871fb967b6f77baf0
GitHub4Jimmy <79417331+GitHub4Jimmy@users.noreply.github.com>
2022-04-21 15:16:14 -0400

    Add files via upload

---
+++ MemberPortal/UnitTests/StateAPITests/TestHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StagwellTech.SEIU.CommonCoreEntities.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StateAPITests
+{
+    public class TestHelper
+    {
+        private readonly SeiuContext _context;
+        public TestHelper()
+        {
+            var builder = new DbContextOptionsBuilder<SeiuContext>();
+            builder.UseInMemoryDatabase(databaseName: "SEIUDbInMemory");
+
+            var dbContextOptions = builder.Options;
+            _context = new SeiuContext(dbContextOptions);
+            // Delete existing db before creating a new one
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+        }
+    }
+}
