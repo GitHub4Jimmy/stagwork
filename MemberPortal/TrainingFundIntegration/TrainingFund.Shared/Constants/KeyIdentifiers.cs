23ff1f16f29a71d7b1663b7bd9673dd9a82667ce
GitHub4Jimmy <79417331+GitHub4Jimmy@users.noreply.github.com>
2022-04-21 15:19:01 -0400

    Add files via upload

---
+++ MemberPortal/TrainingFundIntegration/TrainingFund.Shared/Constants/KeyIdentifiers.cs
@@ -0,0 +1,53 @@
+﻿namespace TrainingFund.Shared.Constants
+{
+    public class KeyIdentifiers
+    {
+        // LOCATIONS
+        public const string LOCATIONS_SEARCH_KEY = "Id_";
+        public const string LOCATIONS_FILTER_KEY = "Filter";
+        public const string LOCATIONS_DIRECTION_KEY = "Address";
+        public const string LOCATIONS_DIRECTION_HOME_KEY = "HomeAddress";
+
+        // COURSE SEARCH
+        public const string COURSE_SEARCH_KEY = "Id_";
+        public const string COURSE_FILTER_KEY = "Filter";
+
+        // MODALS
+        public const string MODAL_COURSE_KEY = "CourseId";
+        public const string MODAL_SESSION_KEY = "SessionId";
+        public const string MODAL_ENROLLED_KEY = "EnrolledId";
+        public const string MODAL_CAREER_TRACK_KEY = "CareerTrackId";
+
+        // PAGES
+        public const string PAGE_FIND_TRAINING_COURSE_KEY = "Courses";
+        public const string PAGE_TRAINING_LOCATIONS_KEY = "Locations";
+        public const string PAGE_SCHOLARSHIP_MAIN_KEY = "Scholarships";
+        public const string PAGE_SCHOLARSHIP_APPLICATION_KEY = "Apply for a scholarship";
+        //public const string PAGE_TRANSCIPTS_KEY = "My Dashboard";
+        public const string PAGE_TRANSCIPTS_KEY = "Transcript";
+        public const string PAGE_CAREER_TRACK_KEY = "Career Tracks";
+        public const string PAGE_HOW_TRAINING_WORKS_KEY = "How Training Works";
+        public const string PAGE_CAREER_SUPPORT_KEY = "Career Support";
+        public const string PAGE_ID_KEY = "PageId";
+
+        // URLS
+        public const string URLS_LOCATION_DIRECTIONS_KEY = "DirectionLink";
+        public const string URLS_ENROLLMENT_DETAILS_KEY = "EnrollmentDetails";
+        public const string URLS_COURSE_DETAILS_KEY = "CourseDetails";
+        public const string URLS_COURSE_ENROLL_KEY = "Enrollment";
+        public const string URLS_COURSE_UNENROLL_KEY = "Enrollment";
+        public const string URLS_COURSE_VIEW_CERTIFCATE_KEY = "CourseViewCertificate";
+        public const string URLS_COURSE_DOWNLOAD_TRANSCRIPT_KEY = "CourseDownloadTranscript";
+        public const string URLS_CAREER_TRACK_DETAILS_KEY = "CareerTrackDetails";
+        public const string URLS_CAREER_TRACK_ENROLL_KEY = "Enrollment";
+        public const string URLS_CAREER_TRACK_UNENROLL_KEY = "Enrollment";
+        public const string URLS_DOCEBO_LAUNCH_COURSE_KEY = "DoceboLaunchCourse";
+        public const string URLS_SCHOLARSHIP_APPLICATION_STATUS_KEY = "ScholarshipApplicationStatus";
+
+        // DOWNLOADS
+        public const string DOWNLOAD_ID_KEY = "Id";
+
+        // SCHOLARSHIP APPLICATION
+        public const string APPLICATION_ID_KEY = "ApplicationId";
+    }
+}
---
+++ MemberPortal/TrainingFundIntegration/TrainingFund.Shared/Constants/LinkIdentifiers.cs
@@ -0,0 +1,37 @@
+﻿namespace TrainingFund.Shared.Constants
+{
+    public class LinkIdentifiers
+    {
+        // PLAIN URL
+        public const string PLAIN_URL = "PlainLinkProvider";
+
+        // LOCATION SEARCH
+        public const string LOCATIONS_DIRECTION = "DirectionsLinkProvider";
+
+        // GENERAL
+        public const string PAGE_LINK = "PageLinkProvider";
+        public const string PAGE_LOCATION_SEARCH_BY_FILTER = "FindTrainingLocationByFilterLinkProvider";
+        public const string PAGE_COURSE_SEARCH_BY_FILTER = "FindTrainingCourseByFilterLinkProvider";
+
+        // MODALS
+        public const string MODAL_COURSE = "CourseModalLinkProvider";
+        public const string MODAL_ENROLLED = "EnrolledModalLinkProvider";
+        public const string MODAL_CAREER_TRACK = "CareerTrackModalLinkProvider";
+        public const string MODAL_SESSIONS = "CourseSessionsModalLinkProvider";
+
+        // DOWNLOADS
+        public const string DOWNLOAD_VIEW_CERTIFICATE = "ViewCertificateLinkProvider";
+        public const string DOWNLOAD_TRANSCRIPT = "DownloadTranscriptLinkProvider";
+
+        // SCHOLARSHIP APPLICATION
+        public const string SCHOLARSHIP_APPLICATION = "ScholarshipApplicationLinkProvider";
+        public const string SCHOLARSHIP_DELETE_APPLICATION = "ScholarshipDeleteApplicationLinkProvider";
+        public const string APPLICATION_STATUS = "ApplicationStatusModalLinkProvider";
+
+        //ENROLL / UNENROLL
+        public const string ENROLL_COURSE = "EnrollmentLinkProvider";
+
+        // LAUNCH COURSE
+        public const string LAUNCH_DOCEBO_COURSE = "LaunchDoceboCourseLinkProvider";
+    }
+}
